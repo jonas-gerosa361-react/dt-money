d7a70cc85c68db5d1ec95d50c8181053438b166f
JGerosa <JGerosa@its.jnj.com>
2022-02-17 23:47:35 -0300

    Finished style of modal new transaction

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,5 +1,8 @@
 import Modal from 'react-modal';
-import { Container } from './styles';
+import closeImg from '../../assets/close.svg';
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
+import { Container, TransactionCategoryContainer } from './styles';
 
 interface NewTransactionModalProps {
   isModalOpen: boolean,
@@ -14,12 +17,40 @@ export function NewTransactionModal({isModalOpen, handleCloseModal}: NewTransact
       overlayClassName='react-modal-overlay'
       className='react-modal-content'
     >
+      <button
+        type="button"
+        onClick={handleCloseModal}
+        className='react-modal-close'
+      >
+        <img src={closeImg} alt="Close modal" />
+      </button>
+
       <Container>
         <h2>New Transaction</h2>
 
         <input type="text" placeholder='Title' />
 
         <input type="number" placeholder='Amount' />
+
+        <TransactionCategoryContainer>
+          <button
+            type='button'
+          >
+            <img src={incomeImg} alt="Income" />
+            <span>
+              Income
+            </span>
+          </button>
+
+          <button
+            type='button'
+          >
+            <img src={outcomeImg} alt="Outcome" />
+            <span>
+              Outcome
+            </span>
+          </button>
+        </TransactionCategoryContainer>
 
         <input type="text" placeholder='Category' />
 
